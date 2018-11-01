5eb2e783796fcf83d52b63d9812aaa49fa270700
Eltheo45 <letheo45@gmail.com>
2018-11-01 10:00:45 -0700

    Avance calculadora

--- Calculadora/js/calculadora.js
+++ Calculadora/js/calculadora.js
@@ -1,6 +1,53 @@
-		var colorAramillo = function(x){
-			x.style.background = "yellow";
+window.onload=function(){
+	var operador=""
+
+	var colorAramillo = function(){
+		this.style.background = "yellow";
+	}
+	var colorBlanco = function(){
+		this.style.background = "white";
+	}
+	var numeros = function(){
+		var valor = this.value;
+
+		if(operador==""){
+			var valorInput = document.getElementById("operando1").value;
+			if (valorInput == "0") {
+				document.getElementById("operando1").value="";
+			}
+			document.getElementById("operando1").value+=valor;
 		}
-		var colorBlanco = function(x){
-			x.style.background = "white";
+		else {
+			var valorInput = document.getElementById("operando2").value;
+			if (valorInput == "0") {
+				document.getElementById("operando2").value="";
+			}
+			document.getElementById("operando2").value+=valor;
 		}
+	}
+	var uno = document.getElementById("uno")
+	uno.addEventListener("click", numeros)
+	var dos = document.getElementById("dos")
+	dos.addEventListener("click",numeros)
+	var tres = document.getElementById("tres")
+	tres.addEventListener("click",numeros)
+	var cuatro = document.getElementById("cuatro")
+	cuatro.addEventListener("click",numeros)
+	var cinco = document.getElementById("cinco")
+	cinco.addEventListener("click",numeros)
+	var seis = document.getElementById("seis")
+	seis.addEventListener("click",numeros)
+	var siete = document.getElementById("siete")
+	siete.addEventListener("click",numeros)
+	var ocho = document.getElementById("ocho")
+	ocho.addEventListener("click",numeros)
+	var nueve = document.getElementById("nueve")
+	nueve.addEventListener("click",numeros)
+	var cero = document.getElementById("cero")
+	cero.addEventListener("click",numeros)
+
+	var operando1 = document.getElementById("operando1")
+	operando1.addEventListener("focus", colorAramillo)
+	operando1.addEventListener("focusout",colorBlanco)
+
+}
